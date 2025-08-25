f08f8ab6ac8af49275aefda7f1f509572f7be042
Raphael Sturm <schlangenkopf@gmx.de>
2025-08-25 02:24:00 +0200

    SnakeHead can move

--- Scripts/SnakeHead.gd
+++ Scripts/SnakeHead.gd
@@ -1,9 +1,18 @@
 extends CharacterBody2D
 
-var speed: float = 10
-var init_vel = Vector2(1, 0) * speed #vel == velocity
+var speed: float = 5
+var init_vel = Vector2.RIGHT * speed #vel == velocity
+var ang_speed: float = deg_to_rad(135)
 
 
+func adjust_direction(_delta) -> void:
+	var mouse_position = get_relative_mouse_position()
+	var rel_mouse_position = mouse_position - position
+	var angle_to_mouse = velocity.angle_to(rel_mouse_position)
+		
+	if angle_to_mouse != 0:
+		velocity = velocity.rotated(sign(angle_to_mouse) * ang_speed * _delta)
+		
 
 func get_relative_mouse_position(): #mouse position relative to node coordinate system
 	var viewportSystem = get_viewport().get_mouse_position() #mouse position from viewport perspective
@@ -18,3 +27,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _physics_process(delta: float) -> void:
+	adjust_direction(delta)
+	move_and_collide(velocity)
